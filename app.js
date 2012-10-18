1a6cdd3a994c576994e166550c42128d906fcf17
Matt Smith <mattr.smith@gmail.com>
2012-10-17 23:27:57 -0700

    Adds facebook oauth

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+	, passport = require('passport')
+	, FacebookStrategy = require('passport-facebook').Strategy;
 
 var app = module.exports = express.createServer();
 
@@ -10,6 +12,8 @@ app.configure(function(){
   app.use(express.session({secret: 'MatthewSmith'}));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+	app.use(passport.initialize());
+	app.use(passport.session());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -22,7 +26,25 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+passport.use(new FacebookStrategy({
+    clientID: "440105096035530",
+    clientSecret: "e45befc93a524072011a4ce920b8bee6",
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(..., function (err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
 app.get('/', routes.index);
+
+app.get('/auth/provider', passport.authenticate('provider'));
+
+app.get('/auth/provider/callback', 
+  passport.authenticate('provider', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
